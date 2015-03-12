39d5bee088473458c8f7c4012e88f534d489b328
Alex Quinlivan <alex@quinlivan.co.nz>
2015-03-13 10:09:01 +1300

    Initial commit from pod lib create

---
+++ Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// FlatBalloon
+#define COCOAPODS_POD_AVAILABLE_FlatBalloon
+#define COCOAPODS_VERSION_MAJOR_FlatBalloon 0
+#define COCOAPODS_VERSION_MINOR_FlatBalloon 1
+#define COCOAPODS_VERSION_PATCH_FlatBalloon 0
+
