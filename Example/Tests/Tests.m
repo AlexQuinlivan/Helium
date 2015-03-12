ea15787c66d9de04213d3d1aee19f28a9d563689
Alex Quinlivan <alex@quinlivan.co.nz>
2015-03-13 10:10:33 +1300

    Lol, tests. I'll write these in my own time.

--- Example/Tests/Tests.m
+++
@@ -1,9 +0,0 @@
-//
-//  FlatBalloonTests.m
-//  FlatBalloonTests
-//
-//  Created by Alex Quinlivan on 03/13/2015.
-//  Copyright (c) 2014 Alex Quinlivan. All rights reserved.
-//
-
-${TEST_EXAMPLE}
