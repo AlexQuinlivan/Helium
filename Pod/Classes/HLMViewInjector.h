4291934bcd39453f28cef1708b17f09f3f16fe8c
Alex Quinlivan <alex@quinlivan.co.nz>
2015-04-07 17:03:08 +1200

    Add the ability to inject an array of views directly into an NSArray

--- Pod/Classes/HLMViewInjector.h
+++ Pod/Classes/HLMViewInjector.h
@@ -9,14 +9,30 @@
 #import <Foundation/Foundation.h>
 
 #define INJECT_VIEW(_view, _tag) \
--(void) inject_$$ ## _view ## _$$ ## _tag:(id) view {\
+-(void) injectview_$$ ## _view ## _$$ ## _tag:(id) view {\
     self._view = view;\
 }
 
 #define INJECT_VIEW_OPTIONAL(_view, _tag) \
--(void) inject_$$ ## _view ## _$$ ## _tag ## _$$optional:(id) view {\
+-(void) injectview_$$ ## _view ## _$$ ## _tag ## _$$optional:(id) view {\
     self._view = view;\
 }
+
+#define INJECT_VIEWS_1(_viewarray, _tag) \
+-(void) injectviews_$$ ## _viewarray ## _$$ ## _tag:(id) views {\
+    self._viewarray = views;\
+}
+
+#define INJECT_VIEWS_2(_viewarray, T1, T2) INJECT_VIEWS_1(_viewarray, T1 ## _$$ ## T2)
+#define INJECT_VIEWS_3(_viewarray, T1, T2, T3) INJECT_VIEWS_2(_viewarray, T1, T2 ## _$$ ## T3)
+#define INJECT_VIEWS_4(_viewarray, T1, T2, T3, T4) INJECT_VIEWS_3(_viewarray, T1, T2, T3 ## _$$ ## T4)
+#define INJECT_VIEWS_5(_viewarray, T1, T2, T3, T4, T5) INJECT_VIEWS_4(_viewarray, T1, T2, T3, T4 ## _$$ ## T5)
+#define INJECT_VIEWS_6(_viewarray, T1, T2, T3, T4, T5, T6) INJECT_VIEWS_5(_viewarray, T1, T2, T3, T4, T5 ## _$$ ## T6)
+#define INJECT_VIEWS_7(_viewarray, T1, T2, T3, T4, T5, T6, T7) INJECT_VIEWS_6(_viewarray, T1, T2, T3, T4, T5, T6 ## _$$ ## T7)
+#define INJECT_VIEWS_8(_viewarray, T1, T2, T3, T4, T5, T6, T7, T8) INJECT_VIEWS_7(_viewarray, T1, T2, T3, T4, T5, T6, T7 ## _$$ ## T8)
+#define INJECT_VIEWS_9(_viewarray, T1, T2, T3, T4, T5, T6, T7, T8, T9) INJECT_VIEWS_8(_viewarray, T1, T2, T3, T4, T5, T6, T7, T8 ## _$$ ## T9)
+#define INJECT_VIEWS_10(_viewarray, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10) INJECT_VIEWS_9(_viewarray, T1, T2, T3, T4, T5, T6, T7, T8, T9 ## _$$ ## T10)
+
 
 @interface HLMViewInjector : NSObject
 
--- Pod/Classes/HLMViewInjector.m
+++ Pod/Classes/HLMViewInjector.m
@@ -9,7 +9,8 @@
 #import "HLMViewInjector.h"
 #import <objc/runtime.h>
 
-static NSString* const HLMViewInjectorPrefix = @"inject_$$";
+static NSString* const HLMViewInjectorPrefix = @"injectview_$$";
+static NSString* const HLMViewArrayInjectorPrefix = @"injectviews_$$";
 static NSString* const HLMInjectorSeperator = @"_$$";
 static NSString* const HLMInjectorSettingOptional = @"optional";
 
@@ -28,35 +29,63 @@ static NSString* const HLMInjectorSettingOptional = @"optional";
         const char* cMethodName = sel_getName(selector);
         NSString* methodName = [NSString stringWithCString:cMethodName encoding:NSUTF8StringEncoding];
         NSString* viewName;
-        NSString* tagName;
-        NSString* injectorSetting;
+        id injectable;
         
         // Check method name matches pattern
-        if (![methodName hasPrefix:HLMViewInjectorPrefix]) {
+        if ([methodName hasPrefix:HLMViewInjectorPrefix]) {
+            
+            // Extract values from method name
+            NSString* injectorSetting;
+            NSString* tagName;
+            methodName = [methodName stringByReplacingOccurrencesOfString:HLMViewInjectorPrefix withString:@""];
+            methodName = [methodName stringByReplacingOccurrencesOfString:@":" withString:@""];
+            NSArray* components = [methodName componentsSeparatedByString:HLMInjectorSeperator];
+            viewName = components[0];
+            tagName = components[1];
+            injectorSetting = (components.count > 2) ? components[2] : nil;
+            
+            // Find the view to inject
+            injectable = [root viewWithTag:tagName.hash];
+            if (!injectable) {
+                if ([injectorSetting isEqualToString:HLMInjectorSettingOptional]) {
+                    return;
+                } else {
+                    @throw [NSException exceptionWithName:@"HLMViewInjectionException"
+                                                   reason:[NSString stringWithFormat:@"Unable to find view with tag `%@`. "
+                                                           @"Did you mean for this to be an optional view? "
+                                                           @"Try INJECT_VIEW_OPTIONAL(prop, tag)", tagName]
+                                                 userInfo:nil];
+                }
+            }
+            
+        } else if ([methodName hasPrefix:HLMViewArrayInjectorPrefix]) {
+            
+            // Extract values from method name
+            methodName = [methodName stringByReplacingOccurrencesOfString:HLMViewArrayInjectorPrefix withString:@""];
+            methodName = [methodName stringByReplacingOccurrencesOfString:@":" withString:@""];
+            NSArray* components = [methodName componentsSeparatedByString:HLMInjectorSeperator];
+            viewName = components[0];
+            
+            // Find the view to inject
+            NSMutableArray* views = [NSMutableArray new];
+            for (int i = 1; i < components.count; i++) {
+                NSString* tagName = components[i];
+                UIView* view = [root viewWithTag:tagName.hash];
+                if (!view) {
+                    @throw [NSException exceptionWithName:@"HLMViewInjectionException"
+                                                   reason:[NSString stringWithFormat:@"Unable to find view with tag `%@`. "
+                                                           @"Did you mean for this to be an optional view? "
+                                                           @"Try INJECT_VIEW_OPTIONAL(prop, tag)", tagName]
+                                                 userInfo:nil];
+                }
+                [views addObject:view];
+            }
+            injectable = views;
+            
+        } else {
             continue;
         }
         
-        // Extract values from method name
-        methodName = [methodName stringByReplacingOccurrencesOfString:HLMViewInjectorPrefix withString:@""];
-        methodName = [methodName stringByReplacingOccurrencesOfString:@":" withString:@""];
-        NSArray* components = [methodName componentsSeparatedByString:HLMInjectorSeperator];
-        viewName = components[0];
-        tagName = components[1];
-        injectorSetting = (components.count > 2) ? components[2] : nil;
-        
-        // Find the view to inject
-        UIView* injectable = [root viewWithTag:tagName.hash];
-        if (!injectable) {
-            if ([injectorSetting isEqualToString:HLMInjectorSettingOptional]) {
-                return;
-            } else {
-                @throw [NSException exceptionWithName:@"HLMViewInjectionException"
-                                               reason:[NSString stringWithFormat:@"Unable to find view with tag `%@`. "
-                                                       @"Did you mean for this to be an optional view? "
-                                                       @"Try INJECT_VIEW_OPTIONAL(prop, tag)", tagName]
-                                             userInfo:nil];
-            }
-        }
         
         // Call setter
         IMP setterImp = [object methodForSelector:selector];
