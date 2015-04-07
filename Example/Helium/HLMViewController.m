7808b09871f3d85ec8054c28c6345103f9ebf846
Alex Quinlivan <alex@quinlivan.co.nz>
2015-04-07 15:05:22 +1200

    Play around with animating certain HLMLayout properties

--- Example/Helium/HLMViewController.m
+++ Example/Helium/HLMViewController.m
@@ -7,16 +7,47 @@
 //
 
 #import "HLMViewController.h"
+#import "HLMLayout.h"
 
 @interface HLMViewController ()
-@property (nonatomic, weak) UIView* example;
+@property (nonatomic, weak) UIView* blueView;
 @end
 
 @implementation HLMViewController
-INJECT_VIEW(example, example)
+INJECT_VIEW(blueView, blue_square)
 
 -(NSString *) layoutResource {
     return @"@view/example_view";
+}
+
+-(void) viewDidLoad {
+    [super viewDidLoad];
+    [self animate];
+}
+
+-(void) animate {
+    __weak typeof(self) weakSelf = self;
+    [UIView animateWithDuration:1
+                          delay:1
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^{
+                         weakSelf.blueView.hlm_layoutHeight = 0;
+                         weakSelf.blueView.hlm_layoutWeight = 0;
+                         [weakSelf.view layoutSubviews];
+                     }
+                     completion:^(BOOL finished) {
+                         [UIView animateWithDuration:1
+                                               delay:1
+                                             options:UIViewAnimationOptionCurveEaseInOut
+                                          animations:^{
+                                              weakSelf.blueView.hlm_layoutHeight = HLMLayoutParamMatch;
+                                              weakSelf.blueView.hlm_layoutWeight = 1;
+                                              [weakSelf.view layoutSubviews];
+                                          }
+                                          completion:^(BOOL finished) {
+                                              [weakSelf animate];
+                                          }];
+                     }];
 }
 
 @end
