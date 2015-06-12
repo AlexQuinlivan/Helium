b9f7bab1ba586ced7025b0da909436c19a79002f
Alex Quinlivan <alex@quinlivan.co.nz>
2015-06-12 16:33:56 +1200

    Complete implementation for the HLMCALayerBridge category for UIViews

--- Pod/Classes/UIView+HLMCALayerBridge.h
+++ Pod/Classes/UIView+HLMCALayerBridge.h
@@ -10,8 +10,25 @@
 
 @interface UIView (HLMCALayerBridge)
 
+@property (nonatomic) BOOL hlm_layerDoubleSided;
+@property (nonatomic) BOOL hlm_layerGeometryFlipped;
+@property (nonatomic) BOOL hlm_layerMasksToBounds;
+@property (nonatomic) CGFloat hlm_layerContentsScale;
+@property (nonatomic) float hlm_layerMinificationFilterBias;
+@property (nonatomic) BOOL hlm_layerNeedsDisplayOnBoundsChange;
+@property (nonatomic) BOOL hlm_layerDrawsAsynchronously;
+@property (nonatomic) BOOL hlm_layerAllowsEdgeAntialiasing;
 @property (nonatomic) CGFloat hlm_layerCornerRadius;
 @property (nonatomic) CGFloat hlm_layerBorderWidth;
 @property (nonatomic, strong) UIColor* hlm_layerBorderColor;
+@property (nonatomic) float hlm_layerOpacity;
+@property (nonatomic) BOOL hlm_layerAllowsGroupOpacity;
+@property (nonatomic) BOOL hlm_layerShouldRasterize;
+@property (nonatomic) CGFloat hlm_layerRasterizationScale;
+@property (nonatomic, strong) UIColor* hlm_layerShadowColor;
+@property (nonatomic) float hlm_layerShadowOpacity;
+@property (nonatomic) CGSize hlm_layerShadowOffset;
+@property (nonatomic) CGFloat hlm_layerShadowRadius;
+@property (nonatomic, strong) NSString* hlm_layerName;
 
 @end
--- Pod/Classes/UIView+HLMCALayerBridge.m
+++ Pod/Classes/UIView+HLMCALayerBridge.m
@@ -10,6 +10,70 @@
 
 @implementation UIView (HLMCALayerBridge)
 
+-(BOOL) hlm_layerDoubleSided {
+    return self.layer.isDoubleSided;
+}
+
+-(void) setHlm_layerDoubleSided:(BOOL) hlm_layerDoubleSided {
+    self.layer.doubleSided = hlm_layerDoubleSided;
+}
+
+-(BOOL) hlm_layerGeometryFlipped {
+    return self.layer.isGeometryFlipped;
+}
+
+-(void) setHlm_layerGeometryFlipped:(BOOL) hlm_layerGeometryFlipped {
+    self.layer.geometryFlipped = hlm_layerGeometryFlipped;
+}
+
+-(BOOL) hlm_layerMasksToBounds {
+    return self.layer.masksToBounds;
+}
+
+-(void) setHlm_layerMasksToBounds:(BOOL) hlm_layerMasksToBounds {
+    self.layer.masksToBounds = hlm_layerMasksToBounds;
+}
+
+-(CGFloat) hlm_layerContentsScale {
+    return self.layer.contentsScale;
+}
+
+-(void) setHlm_layerContentsScale:(CGFloat) hlm_layerContentsScale {
+    self.layer.contentsScale = hlm_layerContentsScale;
+}
+
+-(float) hlm_layerMinificationFilterBias {
+    return self.layer.minificationFilterBias;
+}
+
+-(void) setHlm_layerMinificationFilterBias:(float) hlm_layerMinificationFilterBias {
+    self.layer.minificationFilterBias = hlm_layerMinificationFilterBias;
+}
+
+-(BOOL) hlm_layerNeedsDisplayOnBoundsChange {
+    return self.layer.needsDisplayOnBoundsChange;
+}
+
+-(void) setHlm_layerNeedsDisplayOnBoundsChange:(BOOL) hlm_layerNeedsDisplayOnBoundsChange {
+    self.layer.needsDisplayOnBoundsChange = hlm_layerNeedsDisplayOnBoundsChange;
+}
+
+-(BOOL) hlm_layerDrawsAsynchronously {
+    return self.layer.drawsAsynchronously;
+}
+
+-(void) setHlm_layerDrawsAsynchronously:(BOOL) hlm_layerDrawsAsynchronously {
+    self.layer.drawsAsynchronously = hlm_layerDrawsAsynchronously;
+}
+
+-(BOOL) hlm_layerAllowsEdgeAntialiasing {
+    return self.layer.allowsEdgeAntialiasing;
+}
+
+-(void) setHlm_layerAllowsEdgeAntialiasing:(BOOL) hlm_layerAllowsEdgeAntialiasing {
+    self.layer.allowsEdgeAntialiasing = hlm_layerAllowsEdgeAntialiasing;
+}
+
 -(CGFloat) hlm_layerCornerRadius {
     return self.layer.cornerRadius;
 }
@@ -32,6 +96,78 @@
 
 -(void) setHlm_layerBorderColor:(UIColor *) hlm_layerBorderColor {
     self.layer.borderColor = hlm_layerBorderColor.CGColor;
+}
+
+-(float) hlm_layerOpacity {
+    return self.layer.opacity;
+}
+
+-(void) setHlm_layerOpacity:(float) hlm_layerOpacity {
+    self.layer.opacity = hlm_layerOpacity;
+}
+
+-(BOOL) hlm_layerAllowsGroupOpacity {
+    return self.layer.allowsGroupOpacity;
+}
+
+-(void) setHlm_layerAllowsGroupOpacity:(BOOL) hlm_layerAllowsGroupOpacity {
+    self.layer.allowsGroupOpacity = hlm_layerAllowsGroupOpacity;
+}
+
+-(BOOL) hlm_layerShouldRasterize {
+    return self.layer.shouldRasterize;
+}
+
+-(void) setHlm_layerShouldRasterize:(BOOL) hlm_layerShouldRasterize {
+    self.layer.shouldRasterize = hlm_layerShouldRasterize;
+}
+
+-(CGFloat) hlm_layerRasterizationScale {
+    return self.layer.rasterizationScale;
+}
+
+-(void) setHlm_layerRasterizationScale:(CGFloat) hlm_layerRasterizationScale {
+    self.layer.rasterizationScale = hlm_layerRasterizationScale;
+}
+
+-(UIColor *) hlm_layerShadowColor {
+    return [UIColor colorWithCGColor:self.layer.shadowColor];
+}
+
+-(void) setHlm_layerShadowColor:(UIColor *) hlm_layerShadowColor {
+    self.layer.shadowColor = hlm_layerShadowColor.CGColor;
+}
+
+-(float) hlm_layerShadowOpacity {
+    return self.layer.shadowOpacity;
+}
+
+-(void) setHlm_layerShadowOpacity:(float) hlm_layerShadowOpacity {
+    self.layer.shadowOpacity = hlm_layerShadowOpacity;
+}
+
+-(CGSize) hlm_layerShadowOffset {
+    return self.layer.shadowOffset;
+}
+
+-(void) setHlm_layerShadowOffset:(CGSize) hlm_layerShadowOffset {
+    self.layer.shadowOffset = hlm_layerShadowOffset;
+}
+
+-(CGFloat) hlm_layerShadowRadius {
+    return self.layer.shadowRadius;
+}
+
+-(void) setHlm_layerShadowRadius:(CGFloat) hlm_layerShadowRadius {
+    self.layer.shadowRadius = hlm_layerShadowRadius;
+}
+
+-(NSString *) hlm_layerName {
+    return self.layer.name;
+}
+
+-(void) setHlm_layerName:(NSString *) hlm_layerName {
+    self.layer.name = hlm_layerName;
 }
 
 @end
